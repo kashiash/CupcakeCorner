a456f8dbcca518e12bf6d58b4bc0496dbddb6dca
kashiash <kashiash@gmail.com>
2022-08-06 19:02:49 +0200

    displaying image

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,10 +7,33 @@
 
 import SwiftUI
 
+
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){ phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image")
+        } else {
+            ProgressView()
+        }
+        }
+        .frame(width: 200, height: 200)
     }
 }
 
